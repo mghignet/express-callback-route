3c5bf8bbe906a186eb5e8b966673d810a9d49085
Maxime Ghignet <maxime.ghignet@gmail.com>
2024-10-30 10:23:29 +0100

    feat: add a callback route

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.send('Yeah baby send me those sweet sweet callbacks I\'m ready');
+});
+app.get('/moutiaille', (req, res) => {
+  res.send('Allez Lens et chuche mes prones!');
+});
+
+app.put('/shippings/:shippingId/containers/:containerId/track-pickup', (req, rest, next) => {
+  console.log(`Got a callback from DktPickup on /shippings/${req.params.shippingId}/containers/${req.params.containerId}/track-pickup`);
+  console.log(`Payload\n ${JSON.stringify(req.body, null, 2)}`);
+});
+
+app.listen(port, () => {
+  console.log(`Dkt Pickup test app listening on port ${port}`)
+});
