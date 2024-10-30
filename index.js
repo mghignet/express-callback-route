a772ad6a90fd63aca6debb249a17d8a5c58ee473
Maxime Ghignet <maxime.ghignet@gmail.com>
2024-10-30 10:32:40 +0100

    feat: add a res.send to the PUT callback URL

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ app.get('/moutiaille', (req, res) => {
   res.send('Allez Lens et chuche mes prones!');
 });
 
-app.put('/shippings/:shippingId/containers/:containerId/track-pickup', (req, rest, next) => {
+app.put('/shippings/:shippingId/containers/:containerId/track-pickup', (req, res, next) => {
   console.log(`Got a callback from DktPickup on /shippings/${req.params.shippingId}/containers/${req.params.containerId}/track-pickup`);
   console.log(`Payload\n ${JSON.stringify(req.body, null, 2)}`);
+  res.send(`Got it! shippingId: ${req.params.shippingId}, containerId: ${req.params.containerId}`);
 });
 
 app.listen(port, () => {
